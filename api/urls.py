daaa660af26982f0937d04796c8192e2ae6f34e1
Tomás R. <tomaslopezraigal@gmail.com>
2025-02-09 18:05:36 +0100

    Removed login structure

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet, TaskViewSet, RegisterView
+from .views import NoteViewSet, TaskViewSet
 
 router = DefaultRouter()
 router.register(r'notes', NoteViewSet)
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
     path('', include(router.urls)),
 ]
